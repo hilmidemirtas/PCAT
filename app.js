957741e521c1f37b2ced72930ecf037eb4a378a1
hilmi.demirtas <hilmidemirtas94@gmail.com>
2022-03-24 15:04:53 +0300

    düzenlendi

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const fn = function () {
-  console.log('hello');
-};
+const express = require('express');
 
-fn()
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('merhaba')
+});
+
+app.listen(port, () => {
+    console.log(`Sunucu ${port} portunda...`)
+});
