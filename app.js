9cbe608f9334231267ce792b68c3d115604e62da
hilmi.demirtas <hilmidemirtas94@gmail.com>
2022-03-26 11:20:15 +0300

    'düzenlendi'

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 
+//Template Engine
+app.set("view engine", "ejs");
 
 //MIDDLEWARES
 app.use(express.static('public'));
 
+//ROUTES
+
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('merhaba')
+    /* res.send('merhaba') */
+    res.render('index')
+});
+
+app.get('/about', (req, res) => {
+    /* res.send('merhaba') */
+    res.render('about')
+});
+
+app.get('/add', (req, res) => {
+    /* res.send('merhaba') */
+    res.render('add')
 });
 
 app.listen(port, () => {
