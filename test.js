851696271343240a47309a27a351981417bda960
hilmi.demirtas <hilmidemirtas94@gmail.com>
2022-04-10 14:38:51 +0300

    schema yapısı

---
+++ test.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/pcat-veritabanı');
+
+const PhotoSchema = new Schema({
+  title: String,
+  description: String,
+});
+
+const Photo = mongoose.model('Photo', PhotoSchema);
+
+Photo.create({
+  title: 'Photo Title 1',
+  description: 'Photo description 1 lorem ipsum',
+});
