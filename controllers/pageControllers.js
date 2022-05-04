6682d143de5a31a9631371e4860664eddba7bc39
Hilmi Demirtaş <91216830+hilmidemirtas@users.noreply.github.com>
2022-05-04 22:51:31 +0300

    Update pageControllers.js

--- controllers/pageControllers.js
+++ controllers/pageControllers.js
@@ -1,15 +1,19 @@
+//Avout Page
+
 exports.getAboutPage = (req, res) => {
     /* res.send('merhaba') */
     res.render('about');
   };
 
-
+//Add Page
 
 exports.getAddPage = (req, res) => {
     /* res.send('merhaba') */
     res.render('add');
   };
 
+
+//Edit page
   exports.getEditPage =  async (req, res) => {
     const photo = await  Photo.findOne({_id: req.params.id});
     res.render('edit', {
